92caa8c86bf20eb01bcc30a73a96d19b7d2dd21e
sergejhorvat <sergejhorvat@users.noreply.github.com>
2019-04-08 00:26:53 +0200

    Added Owners List and update controller.

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -1,16 +1,31 @@
 package guru.springframework.sfgpetclinic.controllers;
 
+import guru.springframework.sfgpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/owners") // to use shorted mappings bellow
 @Controller
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    // Constructor injection
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     //@RequestMapping({"/owners","/owners/index","/owners/index.html"})
     @RequestMapping({"","/","/index","/index.html"})
-    public String listOwners(){
 
+    // Bring a model when call a listOwner
+    public String listOwners(Model model){
+
+        // MVC inject modelto Thymeleaf and add attribute owner that finds all owners
+        model.addAttribute("owners", ownerService.findAll());
+
+        // use model in index page of owners
         return  "owners/index";
     }
 }
