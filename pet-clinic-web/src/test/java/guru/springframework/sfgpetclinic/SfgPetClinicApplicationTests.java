ee3fe0a757d4b32dba1388b847ca73c029ad9b49
sergejhorvat <sergejhorvat@users.noreply.github.com>
2019-06-09 22:50:12 +0200

    Project upgrade to JUnit5.

--- pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/SfgPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -1,16 +1,18 @@
 package guru.springframework.sfgpetclinic;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest // configures cpring runner that starts Spring Contex
 public class SfgPetClinicApplicationTests {
 
-    //@Test
-    //public void contextLoads() {
-    //}
+    @Test
+    public void contextLoads() {
+    }
 
 }
